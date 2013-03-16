857b0fba6149d446ec8ed7dd209f4ea5bf843b68
Rafa García <contacto@rafagarcia.net>
2013-03-16 23:22:56 +0100

    Implement insert into node's left side

--- lib/binary-tree/node.rb
+++ lib/binary-tree/node.rb
@@ -1,14 +1,21 @@
 class Node
-  attr_accessor :data
+  attr_accessor :data, :left, :right
 
   def initialize(data=nil)
     @data  = data
+    @left  = nil
+    @right = nil
   end
 
   def insert(node)
     if data.nil?
       self.data = node.data
-      return self
+      return  self
+    end
+
+    if node.data < data
+      self.left = Node.new if left.nil?
+      self.left.insert(node)
     end
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -10,6 +10,8 @@ describe Node do
   end
 
   context "on insert" do
+    let(:tree) { Node.new(9) }
+
     it "should be root if tree is not initialized(root data is nil)" do
       empty_tree = Node.new
       node       = Node.new(5)
@@ -17,6 +19,18 @@ describe Node do
       empty_tree.insert(node)
 
       empty_tree.data.should eq 5
+    end
+
+    it "should be in left side if data is lesser than parent's data" do
+      node_right = Node.new(12)
+      node_left  = Node.new(4)
+
+      tree.insert(node_right)
+      tree.left.should be_nil
+
+
+      tree.insert(node_left)
+      tree.left.data.should eq(node_left.data)
     end
   end
 end
