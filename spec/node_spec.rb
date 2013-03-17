37dc1155c8449e5e78733832d304e300795dd1be
Rafa García <contacto@rafagarcia.net>
2013-03-18 00:30:40 +0100

    Minor change: spec literal

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -118,9 +118,10 @@ describe Node do
       root
     }
 
-    it "should return an array in preorder (root, left and then right)" do
+    it "should return an array in pre-order (root, left and then right)" do
       tree.to_a.should eq [9, [4, [2], [6]], [15, [12], [17]]]
     end
+
   end
 
 end
