243131036348844e71a7b8af114284a51f756a4b
Rafa García <contacto@rafagarcia.net>
2014-08-01 19:10:29 +0200

    Rewrite test using expect instead of should

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -5,7 +5,7 @@ describe Node do
     it "should store data" do
       node = Node.new(42)
 
-      node.data.should eq 42
+      expect(node.data).to eq 42
     end
   end
 
@@ -18,7 +18,7 @@ describe Node do
 
       empty_tree.insert(node)
 
-      empty_tree.data.should eq 5
+      expect(empty_tree.data).to eq 5
     end
 
     it "should be in left side if data is lesser than parent's data" do
@@ -26,11 +26,11 @@ describe Node do
       node_left  = Node.new(4)
 
       tree.insert(node_right)
-      tree.left.should be_nil
+      expect(tree.left).to be_nil
 
 
       tree.insert(node_left)
-      tree.left.data.should eq(node_left.data)
+      expect(tree.left.data).to eq(node_left.data)
     end
 
     it "should be in right side if data is greater than parent's data" do
@@ -38,11 +38,11 @@ describe Node do
       node_left  = Node.new(4)
 
       tree.insert(node_left)
-      tree.right.should be_nil
+      expect(tree.right).to be_nil
 
 
       tree.insert(node_right)
-      tree.right.data.should eq(node_right.data)
+      expect(tree.right.data).to eq(node_right.data)
     end
   end
 
@@ -56,31 +56,31 @@ describe Node do
     it "should return nil if tree is not initialized" do
       empty_tree = Node.new
 
-      empty_tree.search(42).should be_nil
+      expect(empty_tree.search(42)).to be_nil
     end
 
     it "should return bottom left node" do
       node = tree.search(2)
 
-      node.left.should be_nil
-      node.right.should be_nil
-      node.data.should eq 2
+      expect(node.left).to be_nil
+      expect(node.right).to be_nil
+      expect(node.data).to eq 2
     end
 
     it "should return bottom right node" do
       node = tree.search(17)
 
-      node.left.should be_nil
-      node.right.should be_nil
-      node.data.should eq 17
+      expect(node.left).to be_nil
+      expect(node.right).to be_nil
+      expect(node.data).to eq 17
     end
 
     it "should return top node" do
       node = tree.search(9)
 
-      node.left.should_not be_nil
-      node.right.should_not be_nil
-      node.data.should eq 9
+      expect(node.left).to_not be_nil
+      expect(node.right).to_not be_nil
+      expect(node.data).to eq 9
     end
   end
 
@@ -96,18 +96,18 @@ describe Node do
       tree.destroy(9)
 
       tree_values.each do |node_data|
-        tree.search(node_data).should be_nil
+        expect(tree.search(node_data)).to be_nil
       end
     end
 
     it "should not be empty if only destroy a branch(specific node)" do
       tree.destroy(15)
 
-      tree.search(15).should be_nil # Initial node
-      tree.search(12).should be_nil # Children node
-      tree.search(17).should be_nil # Children node
+      expect(tree.search(15)).to be_nil # Initial node
+      expect(tree.search(12)).to be_nil # Children node
+      expect(tree.search(17)).to be_nil # Children node
 
-      tree.search(4).data.should eq 4    # Node in other branch
+      expect(tree.search(4).data).to eq 4    # Node in other branch
     end
   end
 
@@ -119,15 +119,15 @@ describe Node do
     }
 
     it "should return an array in pre-order (root, left and then right)" do
-      tree.to_a.should eq [9, [4, [2], [6]], [15, [12], [17]]]
+      expect(tree.to_a).to eq [9, [4, [2], [6]], [15, [12], [17]]]
     end
 
     it "should return an array in in-order (root, left and then right)" do
-      tree.to_a(:inorder).should eq [[[2], 4, [6]], 9, [[12], 15, [17]]]
+      expect(tree.to_a(:inorder)).to eq [[[2], 4, [6]], 9, [[12], 15, [17]]]
     end
 
     it "should return an array in post-order (left, right and then root)" do
-      tree.to_a(:postorder).should eq [[[2], [6], 4], [[12], [17], 15], 9]
+      expect(tree.to_a(:postorder)).to eq [[[2], [6], 4], [[12], [17], 15], 9]
     end
   end
 
